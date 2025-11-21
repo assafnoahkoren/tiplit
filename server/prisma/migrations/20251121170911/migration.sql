51302fd1cd1d7d3d6684a20edd84aaa350a7a98f
Assaf Noah Koren <assaf.koren@connecteam.com>
2025-11-21 19:15:42 +0200

    .

---
+++ server/prisma/migrations/20251121170911/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[phone]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "phone" TEXT,
+ALTER COLUMN "email" DROP NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ server/src/lib/auth.ts
@@ -0,0 +1,30 @@
+import { Lucia } from 'lucia'
+import { PrismaAdapter } from '@lucia-auth/adapter-prisma'
+import { prisma } from './prisma.js'
+import type { User } from '@prisma/client'
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user)
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      secure: process.env.NODE_ENV === 'production',
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      email: attributes.email,
+      phone: attributes.phone,
+      name: attributes.name,
+    }
+  },
+})
+
+type UserSessionAttributes = Pick<User, 'email' | 'phone' | 'name'>
+
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof lucia
+    DatabaseUserAttributes: UserSessionAttributes
+  }
+}
