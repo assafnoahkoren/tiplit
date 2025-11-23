96ee8d466ff57f28ede3d8c3f4756e3227627e42
Assaf Noah Koren <assaf.koren@connecteam.com>
2025-11-23 13:29:08 -0500

    .

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    codeInspectorPlugin({
+      bundler: 'vite'
+    }),
+    react(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
