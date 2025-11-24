f88e4ccabee77e71953c66666ce8130146f0189f
Assaf Noah Koren <assaf.koren@connecteam.com>
2025-11-23 23:23:30 -0500

    .

--- webapp/src/pages/HomePage.tsx
+++ webapp/src/pages/HomePage.tsx
@@ -16,7 +16,7 @@ export function HomePage() {
   const hasActiveShift = activeSession && new Date(activeSession.endTime) > new Date()
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-background p-4">
+    <div className="flex flex-1 flex-col items-center justify-center bg-background p-4">
       <div className="flex flex-col items-center justify-center space-y-8 max-w-md w-full">
         {!hasActiveShift ? (
           <StartShiftButton onShiftStarted={handleShiftStarted} />
